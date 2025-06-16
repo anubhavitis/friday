2fab46e5e3dfbe5c4fd59d20a1c66bb89129fa1c
abhishek-mvs <saiabhishek546@gmail.com>
2025-06-16 21:44:06 +0530

    Updated the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+echo "Working Directory: $(pwd)"
+echo "Files in schema/:"
+ls -al ./schema
+
+echo "Running db:generate..."
 bun run db:generate
+
+echo "Running db:push..."
 bun run db:push
+
+echo "Starting server..."
 bun run start
